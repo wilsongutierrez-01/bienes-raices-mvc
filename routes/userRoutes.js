fbec3d66333adecc51fcab51bafb5d3507fe5f08
wgutierrez <adolfoguti001@outlook.com>
2024-07-13 19:06:49 -0600

    chore: sign, login and recover password form has been done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { loginForm, signupForm } from '../controllers/userController.js'
+import { loginForm, signupForm, recoverPassword } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.get('/login', loginForm)
 
 router.get('/singup', signupForm)
+
+router.get('/recover-password', recoverPassword)
 
 router.post('/',(req, res) => {
   res.json({msg: 'Post request to the homepage'})
