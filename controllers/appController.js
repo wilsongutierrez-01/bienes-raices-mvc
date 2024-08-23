fb43497c929ac5cf7dc468073092e4c83466d8b4
wilsongutierrez-01 <adolfoguti001@outlook.com>
2024-08-22 21:52:22 -0600

    feat: get home properties

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,14 +2,40 @@ import { Category, Price, Property } from '../models/index.js'
 
 const home = async (req, res) => {
 
-  const [ categories, prices ] = await Promise.all([
+  const [ categories, prices, apartments, houses ] = await Promise.all([
     Category.findAll({raw: true}),
     Price.findAll({raw: true}),
+    Property.findAll({
+      limit: 3,
+      where: { 
+        categoryId: 2 
+      },
+      include: [
+        {
+          model: Price
+        }
+      ],
+      order: [['createdAt', 'DESC']],
+    }),
+    Property.findAll({
+      limit: 3,
+      where: { 
+        categoryId: 5 
+      },
+      include: [
+        {
+          model: Price
+        }
+      ],
+      order: [['createdAt', 'DESC']],
+    })
   ])
   res.render('home', { 
     page: 'Home',
     categories,
-    prices
+    prices,
+    apartments,
+    houses
   })
 }
 
